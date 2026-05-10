b0a9a046ad093bbcec4056724bfc150c5a0cbf75
Anastasia-Zahliada <zahliadaa@gmail.com>
2026-05-10 16:10:26 +0200

    Fix MongoDB connection and error handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,8 @@
+import createHttpError from 'http-errors';
+
 export const errorHandler = (err, _req, res, _next) => {
-  const status = err.status || 500;
+  const isHttpError = createHttpError.isHttpError(err);
+  const status = isHttpError ? err.status : 500;
 
   res.status(status).json({
     message: err.message,
